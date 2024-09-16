23195e0a61196dfbc0769972ead4df1dd43f3cd4
fishcpy <140036207+fishcpy@users.noreply.github.com>
2024-09-16 19:01:31 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,14 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "http://zzz.fishcpy.us.kg:8880/";//your website url
+  let target = "http://zzz.fishcpy.us.kg:8880";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
   //     req.url.startsWith("/banner") ||
   //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
+   //  )
+  //   {
+       target = "http://zzz.fishcpy.us.kg:8880";
   //   }
 
   createProxyMiddleware({
